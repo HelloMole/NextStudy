95a71c713a077232688a62ba8d8e89c9d31a3159
YaoWei <648925074@qq.com>
2017-09-26 16:12:40 +0800

    add feature-2 on branch feature-2

--- app/src/main/java/com/example/liyaowei/nextstudy/MainActivity.java
+++ app/src/main/java/com/example/liyaowei/nextstudy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.liyaowei.nextstudy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,13 +12,15 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView = (TextView)findViewById(R.id.hello_World_Text);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO 在这里加入初始化代码
 
-        TextView textView = (TextView)findViewById(R.id.hello_World_Text);
+
 
         if(textView.getText().toString().equals("HelloWorld")){
             textView.setText("Stupid");
@@ -25,4 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    public void feature2(View view) {
+        if (textView.getText().toString().equals("HelloWorld")) {
+            textView.setText("helloWorld");
+        } else {
+            textView.setText("hhhhhh");
+        }
+    }
+
+
 }

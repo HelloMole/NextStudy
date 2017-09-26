08af95a51333af37c720be10924da6c224bb0ee7
YaoWei <648925074@qq.com>
2017-09-26 15:43:35 +0800

    first Commit repo

---
+++ app/src/main/java/com/example/liyaowei/nextstudy/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.liyaowei.nextstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO 在这里加入初始化代码
+    }
+}
